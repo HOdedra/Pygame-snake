a45fd1a3535594ae99ce258c56ae67fd1434e5b7
Himansu Odedra <himansuodedra@himansus-mbp.home>
2018-05-24 22:34:14 +0100

    Added game over functionality

--- game_1.py
+++ game_1.py
@@ -1,5 +1,5 @@
 import pygame 
-
+import time
 """ 
 Three steps:
 1. event handling
@@ -31,58 +31,85 @@ pygame.display.set_caption('Slither')
 #updating frames etc
 #pygame.display.update()
 
-#game loop
-gameExit= False
 
-#leader of the groups of blocks
-lead_x = display_width/2
-lead_y = display_height/2
-lead_x_change = 0
-lead_y_change = 0
 #FPS
 clock = pygame.time.Clock()
 
 block_size = 10
 FPS = 30
 
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                lead_x_change -= block_size 
-                lead_y_change = 0
-            elif event.key == pygame.K_RIGHT:
-                lead_x_change += block_size
-                lead_y_change = 0
-            elif event.key == pygame.K_UP:
-                lead_y_change -= block_size 
-                lead_x_change = 0
-            elif event.key == pygame.K_DOWN:
-                lead_y_change += block_size
-                lead_x_change = 0
-        """if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                lead_x_change = 0 """
+font = pygame.font.SysFont(None,25)
+
+def message_to_screen(msg,color):
+    screen_text = font.render(msg,True,color)
+    gameDisplay.blit(screen_text,[display_width/2,display_height/2])
+
+def gameLoop():
+    #game loop
+    gameExit= False
+    gameOver = False
     
-    if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
-        gameExit = True
+    #leader of the groups of blocks
+    lead_x = display_width/2
+    lead_y = display_height/2
+    lead_x_change = 0
+    lead_y_change = 0
+    
+    while not gameExit:
         
-    lead_x += lead_x_change
-    lead_y += lead_y_change
+        while gameOver == True:
+            gameDisplay.fill(white)
+            message_to_screen("Game over, press C to play again or Q to quit",red)
+            pygame.display.update()
+            
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameExit = True
+                        gameOver = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    lead_x_change -= block_size 
+                    lead_y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    lead_x_change += block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_UP:
+                    lead_y_change -= block_size 
+                    lead_x_change = 0
+                elif event.key == pygame.K_DOWN:
+                    lead_y_change += block_size
+                    lead_x_change = 0
+            """if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    lead_x_change = 0 """
+        
+        if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
+            gameOver = True
+            
+        
+            
+        lead_x += lead_x_change
+        lead_y += lead_y_change
+    
+        gameDisplay.fill(white)
+        pygame.draw.rect(gameDisplay,black, [lead_x,lead_y,block_size,block_size])
+        
+        #good way to also create a rectangle
+        #gameDisplay.fill(red, rect=[200,200,50,50])
+        
+        pygame.display.update()
+        
+        #FPS
+        clock.tick(FPS)
+    
+    pygame.quit()
+    quit()
 
-    gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay,black, [lead_x,lead_y,block_size,block_size])
-    
-    #good way to also create a rectangle
-    #gameDisplay.fill(red, rect=[200,200,50,50])
-    
-    pygame.display.update()
-    
-    #FPS
-    clock.tick(FPS)
-
-#this uninitializes pygame
-pygame.quit()
-quit()
+gameLoop()
