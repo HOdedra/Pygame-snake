9c17d3d458f5dc3f9b8955519afd31e1959dd703
Himansu Odedra <himansuodedra@Himansus-MacBook-Pro.local>
2018-05-24 20:47:16 +0100

    Remove most hardcoded variables

--- game_1.py
+++ game_1.py
@@ -17,10 +17,12 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 
+display_width = 800
+display_height = 600
 
 #we need a surface like a canvas 
 #this parameter has to be a tuple
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 #title
 pygame.display.set_caption('Slither')
 
@@ -33,12 +35,15 @@ pygame.display.set_caption('Slither')
 gameExit= False
 
 #leader of the groups of blocks
-lead_x = 300
-lead_y = 300
+lead_x = display_width/2
+lead_y = display_height/2
 lead_x_change = 0
 lead_y_change = 0
 #FPS
 clock = pygame.time.Clock()
+
+block_size = 10
+FPS = 30
 
 while not gameExit:
     for event in pygame.event.get():
@@ -46,29 +51,29 @@ while not gameExit:
             gameExit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change -= 10 
+                lead_x_change -= block_size 
                 lead_y_change = 0
             elif event.key == pygame.K_RIGHT:
-                lead_x_change += 10
+                lead_x_change += block_size
                 lead_y_change = 0
             elif event.key == pygame.K_UP:
-                lead_y_change -= 10 
+                lead_y_change -= block_size 
                 lead_x_change = 0
             elif event.key == pygame.K_DOWN:
-                lead_y_change += 10
+                lead_y_change += block_size
                 lead_x_change = 0
         """if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 lead_x_change = 0 """
     
-    if lead_x >= 800 or lead_x < 0 or lead_y >= 600 or lead_y < 0:
+    if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
         gameExit = True
         
     lead_x += lead_x_change
     lead_y += lead_y_change
 
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay,black, [lead_x,lead_y,20,20])
+    pygame.draw.rect(gameDisplay,black, [lead_x,lead_y,block_size,block_size])
     
     #good way to also create a rectangle
     #gameDisplay.fill(red, rect=[200,200,50,50])
@@ -76,7 +81,7 @@ while not gameExit:
     pygame.display.update()
     
     #FPS
-    clock.tick(15)
+    clock.tick(FPS)
 
 #this uninitializes pygame
 pygame.quit()
