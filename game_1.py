3bb4351710c8a898d49df2f0a306ae6263699cd6
Himansu Odedra <himansuodedra@himansus-mbp.home>
2018-05-25 15:10:48 +0100

    Improved collision boundaries so that it works for any size object

--- game_1.py
+++ game_1.py
@@ -113,7 +113,8 @@ def gameLoop():
         
         
         gameDisplay.fill(white)
-        pygame.draw.rect(gameDisplay,red,[randAppleX,randAppleY,block_size,block_size])
+        AppleThickness = 30
+        pygame.draw.rect(gameDisplay,red,[randAppleX,randAppleY,AppleThickness,AppleThickness])
         snake(block_size, snakeList)
         
         
@@ -136,10 +137,18 @@ def gameLoop():
         
         pygame.display.update()
         
+        
+        """
         if lead_x == randAppleX and lead_y == randAppleY:
             randAppleX = round(random.randrange(0,display_width-block_size)/10.0)*10.0
             randAppleY = round(random.randrange(0,display_height-block_size)/10.0)*10.0
             snakeLength += 1
+        """
+        if lead_x >= randAppleX and lead_x <= randAppleX + AppleThickness:
+            if lead_y >= randAppleY and lead_y <= randAppleY + AppleThickness:
+                randAppleX = round(random.randrange(0,display_width-block_size)/10.0)*10.0
+                randAppleY = round(random.randrange(0,display_height-block_size)/10.0)*10.0
+                snakeLength += 1
             
         #FPS
         clock.tick(FPS)
